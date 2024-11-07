d8f09ea391ed40b26448a4f5e85a8af0b7a22120
Dave Oleksy <dave.oleksy@revrobotics.com>
2024-11-07 16:48:58 -0600

    Port Arcade Drive w/ CAN

--- C++/Arcade Drive With CAN/src/main/cpp/Robot.cpp
+++ C++/Arcade Drive With CAN/src/main/cpp/Robot.cpp
@@ -8,33 +8,35 @@
 #include <frc/Joystick.h>
 #include <frc/TimedRobot.h>
 #include <frc/drive/DifferentialDrive.h>
-#include "rev/CANSparkMax.h"
+#include "rev/SparkMax.h"
+#include "rev/config/SparkMaxConfig.h"
 
 class Robot : public frc::TimedRobot {
   /**
-   * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
+   * SPARK MAX controllers are initialized over CAN by constructing a SparkMax object
    * 
    * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
    * first parameter
    * 
    * The motor type is passed as the second parameter. Motor type can either be:
-   *  rev::CANSparkMax::MotorType::kBrushless
-   *  rev::CANSparkMax::MotorType::kBrushed
+   *  kMotorType
+   *  rev::spark::SparkMax::MotorType::kBrushed
    * 
    * The example below initializes four brushless motors with CAN IDs 1, 2, 3 and 4. Change
    * these parameters to match your setup
    */
-  static const int leftLeadDeviceID = 1, leftFollowDeviceID = 2, rightLeadDeviceID = 3, rightFollowDeviceID = 4;
-  rev::CANSparkMax m_leftLeadMotor{leftLeadDeviceID, rev::CANSparkMax::MotorType::kBrushless};
-  rev::CANSparkMax m_rightLeadMotor{rightLeadDeviceID, rev::CANSparkMax::MotorType::kBrushless};
-  rev::CANSparkMax m_leftFollowMotor{leftFollowDeviceID, rev::CANSparkMax::MotorType::kBrushless};
-  rev::CANSparkMax m_rightFollowMotor{rightFollowDeviceID, rev::CANSparkMax::MotorType::kBrushless};
+  static constexpr int kLeftLeadDeviceID = 1, kLeftFollowDeviceID = 2, kRightLeadDeviceID = 3, kRightFollowDeviceID = 4;
+  static constexpr auto kMotorType = rev::spark::SparkMax::MotorType::kBrushless;
+  rev::spark::SparkMax m_leftLeadMotor{kLeftLeadDeviceID, kMotorType};
+  rev::spark::SparkMax m_rightLeadMotor{kRightLeadDeviceID, kMotorType};
+  rev::spark::SparkMax m_leftFollowMotor{kLeftFollowDeviceID, kMotorType};
+  rev::spark::SparkMax m_rightFollowMotor{kRightFollowDeviceID, kMotorType};
 
   /**
-   * In RobotInit() below, we will configure m_leftFollowMotor and m_rightFollowMotor to follow 
-   * m_leftLeadMotor and m_rightLeadMotor, respectively. 
-   * 
-   * Because of this, we only need to pass the lead motors to m_robotDrive. Whatever commands are 
+   * In RobotInit() below, we will configure m_leftFollowMotor and m_rightFollowMotor to follow
+   * m_leftLeadMotor and m_rightLeadMotor, respectively.
+   *
+   * Because of this, we only need to pass the lead motors to m_robotDrive. Whatever commands are
    * sent to them will automatically be copied by the follower motors
    */
   frc::DifferentialDrive m_robotDrive{m_leftLeadMotor, m_rightLeadMotor};
@@ -44,25 +46,44 @@ class Robot : public frc::TimedRobot {
  public:
   void RobotInit() {
     /**
-     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
-     * in the SPARK MAX to their factory default state. If no argument is passed, these
-     * parameters will not persist between power cycles
+     * The ResetMode::kResetSafeParameters constant can be used to reset
+     * the configuration parameters in the SPARK MAX to their factory
+     * default state. If PersistMode::kNoPersistParameters is passed,
+     * these parameters will not persist between power cycles.
      */
-    m_leftLeadMotor.RestoreFactoryDefaults();
-    m_rightLeadMotor.RestoreFactoryDefaults();
-    m_leftFollowMotor.RestoreFactoryDefaults();
-    m_rightFollowMotor.RestoreFactoryDefaults();
-    
+    rev::spark::SparkMaxConfig leftLeadConfig;
+
+    m_leftLeadMotor.Configure(leftLeadConfig,
+      rev::spark::SparkMax::ResetMode::kResetSafeParameters,
+      rev::spark::SparkMax::PersistMode::kNoPersistParameters);
+
+    rev::spark::SparkMaxConfig rightLeadConfig;
+
+    m_rightLeadMotor.Configure(rightLeadConfig,
+      rev::spark::SparkMax::ResetMode::kResetSafeParameters,
+      rev::spark::SparkMax::PersistMode::kNoPersistParameters);
+
     /**
      * In CAN mode, one SPARK MAX can be configured to follow another. This is done by calling
-     * the Follow() method on the SPARK MAX you want to configure as a follower, and by passing
+     * the Follow() method on the SPARK MAX Config you want to configure as a follower, and by passing
      * as a parameter the SPARK MAX you want to configure as a leader.
      * 
      * This is shown in the example below, where one motor on each side of our drive train is
      * configured to follow a lead motor.
      */
-    m_leftFollowMotor.Follow(m_leftLeadMotor);
-    m_rightFollowMotor.Follow(m_rightLeadMotor);
+    rev::spark::SparkMaxConfig leftFollowConfig;
+    leftFollowConfig.Follow(m_leftLeadMotor);
+
+    m_leftFollowMotor.Configure(leftFollowConfig,
+      rev::spark::SparkMax::ResetMode::kResetSafeParameters,
+      rev::spark::SparkMax::PersistMode::kNoPersistParameters);
+
+    rev::spark::SparkMaxConfig rightFollowConfig;
+    rightFollowConfig.Follow(m_rightLeadMotor);
+
+    m_rightFollowMotor.Configure(rightFollowConfig,
+      rev::spark::SparkMax::ResetMode::kResetSafeParameters,
+      rev::spark::SparkMax::PersistMode::kNoPersistParameters);
   }
 
   void TeleopPeriodic() {
